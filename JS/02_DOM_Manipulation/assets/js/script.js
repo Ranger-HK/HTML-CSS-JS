e122926b6fccec4e12f0a13db5fe2876c2888a6a
Ranger-HK <maxravindu99@gmail.com>
2022-11-15 19:30:47 +0530

    Updated

---
+++ JS/02_DOM_Manipulation/assets/js/script.js
@@ -0,0 +1,9 @@
+document.getElementsByTagName("h1")[0].innerText="Hello java Script";
+document.getElementsByTagName("h1")[0].style.fontSize="30px";
+document.getElementsByTagName("h2")[0].innerText="GDSE";
+document.getElementsByTagName("h3")[0].style.color="red";
+document.getElementsByTagName("p")[0].style.backgroundColor="red"
+document.getElementsByTagName("p")[0].style.color="white "
+document.querySelector("input").value="C001";
+
+
