d9bb763a587fffd3caa553f279d0f034d32dc219
Ranger-HK <maxravindu99@gmail.com>
2022-11-16 21:04:08 +0530

    Updated

--- JS/02_DOM_Manipulation/assets/js/script.js
+++ JS/02_DOM_Manipulation/assets/js/script.js
@@ -24,3 +24,24 @@ document.getElementById("btn2").addEventListener("click", function () {
 document.getElementById("img").getAttributeNames();
 document.getElementById("img").getAttribute("src");
 document.getElementById("img").setAttribute("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYIX4fdymadei7FiL-19pxFAWPLEJgQlNEww&usqp=CAU");
+
+
+//set image to click button
+
+document.getElementById("btn3").addEventListener("click", function () {
+    document.getElementById("ipf").value;
+    document.getElementById("img").setAttribute("src", document.getElementById("ipf").value);
+});
+
+// add new li element
+
+document.querySelector("ul").innerHTML;
+document.querySelector("ul").innerHTML = "<li>CMJD</li>";
+document.querySelector("ul").innerHTML = "<li>CMJD</li> <li>GDSE</li>"; // ----------> 1st way
+document.querySelector("ul").innerHTML += "<li>ABSD</li>"; //----------> 1st way
+
+
+document.getElementById("btn4").addEventListener("click", function () {
+    document.getElementById("ipf").value;
+    document.querySelector("ul").innerHTML +="<li>"+document.getElementById("ipf").value+"</li>";
+});
